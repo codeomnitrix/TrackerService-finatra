e3a79cf97ce3d4d25b212b5ba6020c068cc7a9e3
Vinit Tiwari <vinitt@Vinits-MacBook-Air.local>
2018-09-06 21:52:30 +0530

    HelloWorldController with json response

--- src/main/scala/com/afftracker/controller/HelloWorldController.scala
+++ src/main/scala/com/afftracker/controller/HelloWorldController.scala
@@ -3,11 +3,14 @@ package com.afftracker.controller
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
 
+case class Pong(message: String)
+
 class HelloWorldController extends Controller {
   get("/hi") {
     request: Request => {
       info("hi")
-      "Hello " + request.params.getOrElse("name", "unnamed")
+      Pong("Hello " + request.params.getOrElse("name", "unnamed"))
+
     }
   }
 }
